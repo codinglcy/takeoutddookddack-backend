bb62a3582c074fea87aa098897ad9d43e22614c3
codinglcy <codinglcy@gmail.com>
2023-02-15 16:51:48 +0900

    :tada: java springboot 초기 세팅

---
+++ src/main/java/lcy/takeoutddookddack/TakeoutddookddackApplication.java
@@ -0,0 +1,13 @@
+package lcy.takeoutddookddack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutddookddackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeoutddookddackApplication.class, args);
+	}
+
+}
