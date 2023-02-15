82267e4995595bcb3a8fdf85c84356cfa5887b97
codinglcy <codinglcy@gmail.com>
2023-02-15 17:08:23 +0900

    :pencil: 연동 테스트

---
+++ src/main/java/lcy/takeoutddookddack/Controller/HelloWorldControllerTest.java
@@ -0,0 +1,12 @@
+package lcy.takeoutddookddack.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldControllerTest {
+    @GetMapping("api/hello")
+    public String test(){
+        return "Hello, world!";
+    }
+}
