ff977bdea1b258e311bfb36816f70abd647e6fcc
codinglcy <codinglcy@gmail.com>
2023-02-17 20:36:29 +0900

    🎨 재배포

--- src/main/java/lcy/takeoutddookddack/Controller/HelloWorldControllerTest.java
+++ src/main/java/lcy/takeoutddookddack/Controller/HelloWorldControllerTest.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldControllerTest {
     @GetMapping("api/hello")
     public String test(){
-        return "Hello, world!!";
+        return "Hello, world!";
     }
 }
