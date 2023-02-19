c49530cb3264920b94c5d34c1a17ccd139f92623
codinglcy <codinglcy@gmail.com>
2023-02-19 15:08:02 +0900

    🎨 포트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Dserver.port=8080 $JAR_FILE > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dserver.port=8000 $JAR_FILE > /dev/null 2> /dev/null < /dev/null &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
