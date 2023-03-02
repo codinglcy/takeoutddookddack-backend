77ae9c875888721fe89b5b183e39e75a08f8bbfb
codinglcy <codinglcy@gmail.com>
2023-03-02 23:55:19 +0900

    🎨 jwtProvider 수정
    - 폴더명
    - refresh토큰 주석처리

--- src/main/java/lcy/takeoutddookddack/jwt/JwtProvider.java
+++ src/main/java/lcy/takeoutddookddack/jwt/JwtProvider.java
@@ -1,21 +1,14 @@
-package lcy.takeoutddookddack.utils;
+package lcy.takeoutddookddack.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
 import lcy.takeoutddookddack.error.CustomException;
 import lcy.takeoutddookddack.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidParameterException;
 import java.security.Key;
 import java.util.Date;
 
@@ -25,22 +18,14 @@ public class JwtProvider {
     @Value("${jwt.secret.key}")
     private String salt;
 
-    private final long accessExp = 1000L * 60 * 60; //1시간
-    private final long refreshExp = 1000L * 60 * 60 * 24 * 30; //1개월
+    private final long accessExp = 1000L * 60 * 60 * 13; //13시간
+    private final long refreshExp = 1000L * 60 * 60 * 24 * 30 * 3; //3개월
 
     private Key getSecretKey(String salt){
         return Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
     }
 
     public String createAccessToken(String id, String sellerId){
-        return createToken(id, sellerId, accessExp);
-    }
-
-    public String createRefreshToken(String id, String sellerId){
-        return createToken(id, sellerId, refreshExp);
-    }
-
-    private String createToken(String id, String sellerId, Long exp){
         Claims claims = Jwts.claims();
         claims.put("sellerId", sellerId);
         claims.put("id", id);
@@ -48,10 +33,19 @@ public class JwtProvider {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + exp))
+                .setExpiration(new Date(now.getTime() + accessExp))
                 .signWith(getSecretKey(salt), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+//    public String createRefreshToken(){
+//        Date now = new Date();
+//        return Jwts.builder()
+//                .setIssuedAt(now)
+//                .setExpiration(new Date(now.getTime() + refreshExp))
+//                .signWith(getSecretKey(salt), SignatureAlgorithm.HS256)
+//                .compact();
+//    }
 
     public Claims validateToken(String token){
         try{
@@ -65,7 +59,7 @@ public class JwtProvider {
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
-        }catch (Exception e){
+        }catch (JwtException e){
             throw new CustomException(ErrorCode.INVALID_TOKEN);
         }
     }
