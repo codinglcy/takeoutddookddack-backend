471cc55abd3e4b7f9ea3455e08822503a269e4a4
codinglcy <codinglcy@gmail.com>
2023-03-21 01:21:22 +0900

    🎨 빈 등록

--- src/main/java/lcy/takeoutddookddack/jwt/SecurityUtil.java
+++ src/main/java/lcy/takeoutddookddack/jwt/SecurityUtil.java
@@ -2,7 +2,9 @@ package lcy.takeoutddookddack.jwt;
 
 import io.jsonwebtoken.Claims;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityUtil {
     public Claims getCurrentSeller(){
         final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
