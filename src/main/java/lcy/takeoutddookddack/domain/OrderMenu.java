3f001e11b3dc2a05bdff94f42ea3a21157952a0b
codinglcy <codinglcy@gmail.com>
2023-03-25 19:57:22 +0900

    🎨 생성자 오류 수정

--- src/main/java/lcy/takeoutddookddack/domain/OrderMenu.java
+++ src/main/java/lcy/takeoutddookddack/domain/OrderMenu.java
@@ -2,10 +2,12 @@ package lcy.takeoutddookddack.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Getter
+@NoArgsConstructor
 public class OrderMenu {
     private String item;
     private int price;
